87865fa5a13d6847f754641f9da461ef7fd57416
ThanhNhan301102 <phamthanhnhan30112002@gmail.com>
2024-04-13 21:07:03 +0700

    gold

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int health;
+    [SerializeField] private GameObject goldPrefab;
+    private bool flag = false;
 
     public void Hit(int damage)
     {
         health -= damage;
-        if (health <= 0)
+        if (health <= 0 && flag == false)
+        {
+            flag = true;
             Destroy(gameObject);
+            GameObject newGold = Instantiate(goldPrefab, transform.position, Quaternion.identity);
+            GameMng.currentEnemy++;
+        }
+            
     }
 }
